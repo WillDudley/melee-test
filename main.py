58940552fb9a6c5a8f1832cce07ebb4522af922e
will <will2346@live.co.uk>
2022-11-05 01:08:32 +0000

    rename to gymn/pz conventions

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ players = [CPU(enums.Character.FOX, 9, press_start=True), CPU(enums.Character.FO
 env = MeleeEnv(os.path.expanduser('~/.melee/SSBM.ciso'), players, fast_forward=True)
 
 episodes = 10; reward = 0
-env.start()
+env.start_emulator()
 
 for episode in range(episodes):
-    gamestate, done = env.setup(enums.Stage.FOUNTAIN_OF_DREAMS)
+    gamestate, done = env.reset(enums.Stage.FOUNTAIN_OF_DREAMS)
     while not done:
         for i in range(len(players)):
             players[i].act(gamestate)
